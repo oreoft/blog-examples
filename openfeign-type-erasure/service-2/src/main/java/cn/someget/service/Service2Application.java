a773f4577700c78a078286ae17bf7711c0870b02
oreoft <ft1216@foxmail.com>
2021-10-31 02:43:25 +0800

    add(openfeign-type-erasure): 更新openfeign类型擦除的神坑

---
+++ openfeign-type-erasure/service-2/src/main/java/cn/someget/service/Service2Application.java
@@ -0,0 +1,17 @@
+package cn.someget.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 模拟service2
+ * @author oreoft
+ */
+@SpringBootApplication
+public class Service2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Service2Application.class, args);
+  }
+
+}
